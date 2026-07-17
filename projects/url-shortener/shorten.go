de0ac87404b76642935f75b90da3c213bac36145
Rash <orashusedmund@gmail.com>
2026-07-17 15:33:43 +0100

    wrote shortener with uniquer check for unique keys, temporal store, and mutext locks

--- projects/url-shortener/shorten.go
+++ projects/url-shortener/shorten.go
@@ -1,14 +1,96 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
 )
 
+var (
+	store = make(map[string]string) // shortCode -> longUrl
+	mu    sync.RWMutex
+)
+
+const consoleSpace string = "---------------------------------"
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateShortCode(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+const maxRetries = 10
+
+func generateUniqueShortCode(n int, longUrl string) (string, error) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for i := 0; i < maxRetries; i++ {
+		code := generateShortCode(n)
+		if _, exists := store[code]; !exists {
+			store[code] = longUrl // reserves shortcode while we still hold the lock
+			return code, nil
+		}
+	}
+
+	return "", errors.New("Unique Code could not be generated")
+}
+
+// POST /shorten?url=https://www.example.com/path123?query=123#fragment123
+func shortenHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(consoleSpace)
+	log.Println("Handling Shorten...")
+
+	longUrl := req.URL.Query().Get("url")
+
+	if longUrl == "" {
+		http.Error(w, "Missing URL Parameter", http.StatusBadRequest)
+		return
+	}
+
+	code, err := generateUniqueShortCode(6, longUrl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "http://localhost:8080/%s", code)
+}
+
+// GET /<shortcode>
+func redirectHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(consoleSpace)
+	log.Println("Handling Redirect...")
+
+	code := req.URL.Path[1:] // strip leading "/"
+
+	mu.RLock()
+	longUrl, ok := store[code]
+	mu.RUnlock()
+
+	if !ok {
+		http.NotFound(w, req)
+		return
+	}
+
+	fmt.Println("Reading url:", req.URL)
+	fmt.Println("Redirecting to", longUrl)
+
+	http.Redirect(w, req, longUrl, http.StatusFound)
+}
+
 func main() {
-	args := os.Args[1:]
+	// could still use http.HandleFunc("/shorten", shortenHandler) this this would enforce only "POST" verb
+	http.HandleFunc("POST /shorten", shortenHandler)
+	http.HandleFunc("GET /", redirectHandler)
 
-	fmt.Println("Hello, World!")
-
-	fmt.Println(args, len(args))
+	log.Println("\nListening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
